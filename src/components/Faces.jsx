285b6c835c5db2d886f28a158fa6b1a7331de27c
bamidelemo <bamidelemo@gmail.com>
2024-12-01 04:38:10 +0100

    .

--- src/components/Faces.jsx
+++ src/components/Faces.jsx
@@ -8,7 +8,6 @@ import user5 from "../assets/user5.png";
 function Faces() {
   return (
     <div class="text-left mb-6 w-fit mx-auto lg:mx-0 flex space-x-1">
-      <div class="mt-1 md:mt-1">Read by 1,003+ students</div>
       <div class="flex -space-x-2">
         <img
           src={user1}
@@ -36,6 +35,7 @@ function Faces() {
           class="w-8 h-8 md:w-8 md:h-8 rounded-full border-2 border-white"
         />
       </div>
+      <div class="mt-1 md:mt-1">1,003+ students signed up</div>
     </div>
   );
 }
