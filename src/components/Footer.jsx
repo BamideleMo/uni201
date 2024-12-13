678872d679ed3886b6476033c42ee25eae29e9a0
bamidelemo <bamidelemo@gmail.com>
2024-12-13 11:35:48 +0100

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="size-6 inline"
+            class="size-5 inline"
           >
             <path
               stroke-linecap="round"
