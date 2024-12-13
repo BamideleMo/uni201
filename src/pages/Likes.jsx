2e74b141a3b4c131020602b6abeb288dc7875adb
bamidelemo <bamidelemo@gmail.com>
2024-12-13 11:01:54 +0100

    .

--- src/pages/Likes.jsx
+++ src/pages/Likes.jsx
@@ -11,10 +11,7 @@ function Likes() {
     <MetaProvider>
       <Title>My UNI201 likes | www.uni201.com.ng</Title>
       <Link rel="canonical" href="https://techinjos.com.ng/" />
-      <Meta
-        name="description"
-        content="All posts I have liked on UNI201."
-      />
+      <Meta name="description" content="All posts I have liked on UNI201." />
       <div>
         <Header />
         <div class="pt-24 md:pt-28">
@@ -22,10 +19,10 @@ function Likes() {
             <div class="content md:w-10/12 lg:w-7/12 2xl:w-6/12 mx-auto space-y-3">
               <div class="bg-white p-2 md:p-6">
                 <h2 class="text-lg md:text-xl border-b-2 border-black pb-2">
-                  <span class="bg-blue-300 p-1">Likes</span>
+                  <span class="bg-blue-300 p-1">My Likes</span>
                 </h2>
                 <h2 class="mt-8 text-xl md:text-2xl leading-tight font-bold">
-                  My Liked Posts
+                  All the Posts I've Liked!
                 </h2>
                 <div class="my-4 space-y-6 text-base">
                   <p class="text-gray-400">No results found</p>
