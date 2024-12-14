0f39fbefbdd017c470bbb50aea3b8373ecfea81f
bamidelemo <bamidelemo@gmail.com>
2024-12-14 01:54:52 +0100

    .

--- src/pages/Likes.jsx
+++ src/pages/Likes.jsx
@@ -39,7 +39,7 @@ function Likes() {
                 <div class="content md:w-10/12 lg:w-7/12 2xl:w-6/12 mx-auto space-y-3">
                   <div class="bg-white p-2 md:p-6">
                     <h2 class="text-lg md:text-xl border-b-2 border-black pb-2">
-                      <span class="bg-yellow-300 p-1">My Liked Posts</span>
+                      <span class="bg-purple-300 p-1">My Liked Posts</span>
                     </h2>
                     <h2 class="mt-8 text-xl md:text-2xl leading-tight font-bold">
                       All the Posts I've Liked!
