f7c9d031da213567f6f36438c21bcce64e209e3a
bamidelemo <bamidelemo@gmail.com>
2025-02-04 08:13:19 +0100

    .

--- src/pages/Issue.jsx
+++ src/pages/Issue.jsx
@@ -501,7 +501,7 @@ function Issue() {
                             <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                           </svg>
                           <span class="mt-0.5">
-                            {likers.length} Like {likers.length > 1 ? "s" : ""}
+                            {likers.length} Like{likers.length > 1 ? "s" : ""}
                           </span>
                         </div>
                         <div class="text-gray-400 flex space-x-1">
@@ -559,12 +559,7 @@ function Issue() {
                               </span>
                             }
                           >
-                            <span
-                              onClick={() => {
-                                doLike(resource().issue.issue_number);
-                              }}
-                              class="border-2 border-black h-14 px-3 rounded-lg flex items-center justify-between cursor-not-allowed text-black"
-                            >
+                            <span class="border-2 border-black h-14 px-3 rounded-lg flex items-center justify-between cursor-not-allowed text-black">
                               <span>
                                 <span class="lg:hidden">Liked this</span>
                                 <span class="hidden lg:block">
@@ -583,12 +578,7 @@ function Issue() {
                           </Show>
                         }
                       >
-                        <span
-                          onClick={() => {
-                            doLike(resource().issue.issue_number);
-                          }}
-                          class="border-2 border-black h-14 px-3 rounded-lg flex items-center justify-between cursor-not-allowed text-black"
-                        >
+                        <span class="border-2 border-black h-14 px-3 rounded-lg flex items-center justify-between cursor-not-allowed text-black">
                           <span>
                             <span class="lg:hidden">Liking.. .</span>
                             <span class="hidden lg:block">
